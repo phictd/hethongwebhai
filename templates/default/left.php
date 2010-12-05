bf493783d4e038c730fffe85e98548f6a1fd7b02
tranhuuthoicn07b <tranhuuthoicn07b@a8a82d7d-1049-7536-f1cc-18fe8475b038>
2010-12-05 21:39:21 +0000

    thêm images, các trang phụ index.php

--- templates/default/left.php
+++ templates/default/left.php
@@ -1,16 +1,57 @@
-    <div id="main">
+﻿<div id="main">
     	<div id="left">
-        	<h1>Main Menu</h1>
-            <ul>
-            	<li><a href="index.php?module=loaihang">Loại Hàng </a></li>
-            	<li><a href="index.php?module=congty">Công Ty</a></li>
-            	<li><a href="index.php?module=hanghoa">Hàng Hóa</a></li>
-                <li><a href="index.php?module=xuatkho">Xuất Kho</a></li>  
-            	<li><a href="index.php?module=nhapkho">Nhập Kho</a></li>
-            	<li><a href="index.php?module=donhang">Đơn Hàng</a></li>
-                <li><a href="index.php?module=user">User</a></li>
-				<li><a href="../index.php">Trang Chủ</a></li>
-                <li><a href="../libraries/logout.php">Thoát</a></li>                                                                              
-            </ul>
+        	<h1>Sản Phẩm</h1>
+           <ul id="verticalmenu">
+             <?php
+				require_once('libraries/oop.php');
+				require_once('libraries/loaihang.php');
+				$a= new LoaiHang;
+				$data=$a->listloaihang();
+				foreach($data as $item){
+					echo "<li><a href='#'>$item[TenLoai] </a>";
+					echo "<ul>";
+					$a->set_idloai($item[idLoaiHang]);
+					$datact=$a->listcongty();
+					foreach($datact as $itemct){					
+						echo "<li><a href='#'>$itemct[TenCongTy]</a></li>";
+					}
+					echo "</ul>";
+					echo "</li>";
+				}
+			?>
+           </ul>
+           <div class="img">
+           	<img src="images/icons/hotro.png" width="150" />
+           </div>
+           <div class="img1">
+               Mr.Dũng (Kinh doanh)
+              <a href="#"><img src="images/icons/online(1).gif"/></a>
+           </div>
+           <div class="img1">
+               Mr.Tùng (Kinh doanh)
+              <a href="#"><img src="images/icons/online(1).gif"/></a>
+           </div>
+           <div class="img1">
+               Mr.An (Kinh Doanh)
+              <a href="#"><img src="images/icons/online(1).gif"/></a>
+           </div>
+           <div class="img1">
+               Support by English
+                <a href="#"><img src="images/icons/online(1).gif"/></a>
+           </div>
+          <div class="img1">
+          	<a href="#"><img src="images/laptop/hinh/h1.jpg" width="145" height="100" name="h" style="filter:blendTrans(duration=3)" /></a>
+          </div>
+          <div class="img1">
+          	<a href="#"><img src="images/laptop/anh/a1.jpg" width="145" height="100" name="h1" style="filter:chroma(Color = #000000) DropShadow(Color=#FF0000, OffX=2, OffY=2, Positive=1)" /></a>
+          </div>
+          <div class="img1">
+          	<a href="#"><img src="images/laptop/hinh1/b1.jpg" width="145" height="100" name="h2" style="filter:blendTrans(duration=3)" /></a>
+          </div>
+          <div class="img1">
+          	<a href="#"><img src="images/laptop/727_mainimage.jpg" width="145" /></a>
+          </div>
+          <div class="img1">
+          	<a href="#"><img src="images/laptop/626.jpg" width="145" /></a>
+          </div>
         </div>
-        <div id="info">
